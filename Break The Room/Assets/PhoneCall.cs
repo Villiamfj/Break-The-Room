329e7105b663e3b276eb8b39edd32cc4e049112f
Villiamfj <40019337+Villiamfj@users.noreply.github.com>
2019-04-15 22:54:14 +0200

    Mere lyd, døre og andet

---
+++ Break The Room/Assets/PhoneCall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneCall : MonoBehaviour
+{
+    AudioSource Audios;
+    public AudioClip voice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Audios = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            if (transform.parent.name == "RightHand" || transform.parent.name == "LeftHand")
+            {
+                Audios.clip = voice;
+            }
+        }catch
+        {
+            //Nothing
+        }
+    }
+}
